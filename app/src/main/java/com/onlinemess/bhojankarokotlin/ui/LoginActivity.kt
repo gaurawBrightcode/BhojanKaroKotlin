3c2ac6aaa53cdd6688e8ccb6aa7640b018129f8d
gaurawBrightcode <gauraw.kumar@brightcodess.com>
2022-01-28 14:40:00 +0530

    dsasd

---
+++ app/src/main/java/com/onlinemess/bhojankarokotlin/ui/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.onlinemess.bhojankarokotlin.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.onlinemess.bhojankarokotlin.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
